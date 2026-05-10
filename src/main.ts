82f0780121258b032b2d92dca5a03e3662cb4a28
Arturo Guerrero <aguerrerog@losportales.com.pe>
2026-05-10 02:29:45 -0500

    actualizar

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
-import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 
-if (environment.production) {
-  enableProdMode();
-}
-
-bootstrapApplication(AppComponent, {providers: [provideZoneChangeDetection()]}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideZoneChangeDetection()],
+}).catch((err) => console.error(err));
