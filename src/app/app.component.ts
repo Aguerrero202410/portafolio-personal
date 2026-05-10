7d6bc48d7a516c16e71a5dbc8af0b817516dbc30
Arturo Guerrero <aguerrerog@losportales.com.pe>
2026-05-10 01:31:43 -0500

    actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,10 @@ declare global {
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: false
 })
 export class AppComponent implements AfterViewInit {
   readonly title = 'aguerrero-portafolio';
